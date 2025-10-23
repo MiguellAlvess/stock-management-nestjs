ec85265193117aed56881ff211dad514a8c10640
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-10-23 14:44:30 -0300

    feat: implement stock inputs management with creation logic and validation

--- src/stock-inputs/stock-inputs.service.ts
+++ src/stock-inputs/stock-inputs.service.ts
@@ -1,11 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import { CreateStockInputDto } from './dto/create-stock-input.dto';
-import { UpdateStockInputDto } from './dto/update-stock-input.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { NotFoundError } from 'src/products/errors';
 
 @Injectable()
 export class StockInputsService {
-  create(createStockInputDto: CreateStockInputDto) {
-    return 'This action adds a new stockInput';
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createStockInputDto: CreateStockInputDto) {
+    const product = await this.prismaService.product.findUnique({
+      where: {
+        id: createStockInputDto.product_id,
+      },
+    });
+    if (!product) {
+      throw new NotFoundError('Product not found');
+    }
+    const result = await this.prismaService.$transaction([
+      this.prismaService.stockInput.create({
+        data: {
+          productId: createStockInputDto.product_id,
+          quantity: createStockInputDto.quantity,
+          date: createStockInputDto.date,
+        },
+      }),
+      this.prismaService.product.update({
+        where: {
+          id: createStockInputDto.product_id,
+        },
+        data: {
+          quantity: {
+            increment: createStockInputDto.quantity,
+          },
+        },
+      }),
+    ]);
+    return result[0];
   }
 
   findAll() {
@@ -14,13 +44,5 @@ export class StockInputsService {
 
   findOne(id: number) {
     return `This action returns a #${id} stockInput`;
-  }
-
-  update(id: number, updateStockInputDto: UpdateStockInputDto) {
-    return `This action updates a #${id} stockInput`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} stockInput`;
   }
 }
